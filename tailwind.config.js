785afa7a70316c969e76646f0acaab8b772ab64a
weslley <wesleyyrocha20@gmail.com>
2024-03-14 20:56:21 -0300

    Project-inst-tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        "blue-facebook": "#1877F2",
+        "text-blue-facebook": "#385185",
+  
+        
+      },
+    },
+    spacing: {
+      '480': '480px',
+      '500': '500px',
+      '380': '380px',
+      '500': '500px',
+      '50': '50px',
+      '60': '60px',
+      '20': '20px',
+      '100': '100px',
+      '10': '10px',
+      '150':'150px',
+      '30': '30px',
+      '250': '250px',
+
+    }
+  },
+  plugins: [],
+}
+
